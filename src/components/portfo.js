d5288d1d4e4e77511c581174ce40e1888f5bff38
sarafotros <sarafotros@gmail.com>
2020-05-26 21:33:45 +0100

    modal photo

--- src/components/portfo.js
+++ src/components/portfo.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, Modal } from '@material-ui/core';
 
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		padding: 15,
 		display: 'flex',
-        flexDirection: 'column',
-        marginBottom: 15,
+		flexDirection: 'column',
+		marginBottom: 15,
 	},
 	image: {
-        width: '100%',
-       
+		width: '100%',
+		cursor: 'pointer',
+		'&:hover': {
+			transform: 'rotate(360deg)',
+			transition: '1s',
+		},
 	},
 	title: {
-        textAlign: 'left',
-        cursor: 'pointer',
+		textAlign: 'left',
+		cursor: 'pointer',
 		'& a': {
 			textDecoration: 'none',
 			color: '#FFF',
@@ -27,15 +31,32 @@ const useStyles = makeStyles((theme) => ({
 			},
 		},
 	},
+	modalImage: {
+		width: '100%',
+		height: '100%',
+	},
+	modal: {
+		width: '60%',
+		height: '80%',
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		marginTop: 80,
+		[theme.breakpoints.down('xs')]: {
+			width: '90%',
+			height: '50%',
+		},
+	},
 }));
 
 
 
 export default function Portfo({image, title, desc, href}) {
     const classes = useStyles()
+    const [open, setOpen]= useState(false)
     return (
+        <>
 			<Grid item lg={4} className={classes.root}>
-				<img className={classes.image} src={image} alt={title} />
+				<img onClick={()=> setOpen(true)} className={classes.image} src={image} alt={title} />
 				<Typography variant="h5" className={classes.title}>
 					<a
 						href={href}
@@ -46,6 +67,10 @@ export default function Portfo({image, title, desc, href}) {
 					</a>
 				</Typography>
 				<Typography variant="body1">{desc}</Typography>
-			</Grid>
+            </Grid>
+            <Modal className={classes.modal} open={open} onClose={()=> setOpen(false)}>
+				<img className={classes.modalImage} src={image} alt={title} />
+        </Modal>
+        </>
 		);
 }
