68ee7891409e5ba8d7b04703075f658fbceb7b5c
sarafotros <sarafotros@gmail.com>
2020-05-29 10:55:09 +0100

    Updates

---
+++ precache-manifest.d25b706ddfffd6e326435979e143297c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72085da34750590f41e9c8b4ae8e310",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f33295df8af0fe8f121",
+    "url": "/static/css/main.4e206d0c.chunk.css"
+  },
+  {
+    "revision": "0c5ff9ca64c8fddeb683",
+    "url": "/static/js/2.1cfbade2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1cfbade2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f33295df8af0fe8f121",
+    "url": "/static/js/main.33cc1c35.chunk.js"
+  },
+  {
+    "revision": "2e567fc4448202a0f592",
+    "url": "/static/js/runtime-main.a986343f.js"
+  },
+  {
+    "revision": "244a401eb43afb0fe739f8dcd8530ae4",
+    "url": "/static/media/IRANSansWeb.244a401e.ttf"
+  },
+  {
+    "revision": "50f4bba2f37081daeca8c423abdced8b",
+    "url": "/static/media/IRANSansWeb.50f4bba2.woff"
+  },
+  {
+    "revision": "70a5f97c18c45e8ef620a374be61c6c3",
+    "url": "/static/media/IRANSansWeb.70a5f97c.eot"
+  },
+  {
+    "revision": "9d38733b8a7b9153f2bf9bfe476ef8e3",
+    "url": "/static/media/IRANSansWeb.9d38733b.woff2"
+  },
+  {
+    "revision": "465387eeda5050a622ba869f13e53119",
+    "url": "/static/media/IRANSansWeb_Black.465387ee.eot"
+  },
+  {
+    "revision": "63911f38b2d0bb0650d18555d793dad3",
+    "url": "/static/media/IRANSansWeb_Black.63911f38.woff2"
+  },
+  {
+    "revision": "7174439308f3d61c0c1f67fd3e052c78",
+    "url": "/static/media/IRANSansWeb_Black.71744393.ttf"
+  },
+  {
+    "revision": "818f7796aeb3cc761b74334bb0b9aa35",
+    "url": "/static/media/IRANSansWeb_Black.818f7796.woff"
+  },
+  {
+    "revision": "2f6f164c3e1f1329cc658b3c725bfd98",
+    "url": "/static/media/IRANSansWeb_Bold.2f6f164c.woff2"
+  },
+  {
+    "revision": "9471804b0f92127716fabbbb2c6647e5",
+    "url": "/static/media/IRANSansWeb_Bold.9471804b.ttf"
+  },
+  {
+    "revision": "a65cbf91ec2b253357f7621900ef29a4",
+    "url": "/static/media/IRANSansWeb_Bold.a65cbf91.eot"
+  },
+  {
+    "revision": "d44408d0941f077f60b1306e1331e806",
+    "url": "/static/media/IRANSansWeb_Bold.d44408d0.woff"
+  },
+  {
+    "revision": "7c207019aa62aa4778add0c2b3372720",
+    "url": "/static/media/IRANSansWeb_Light.7c207019.woff2"
+  },
+  {
+    "revision": "9d7efb918cab94704068d84361d3f898",
+    "url": "/static/media/IRANSansWeb_Light.9d7efb91.eot"
+  },
+  {
+    "revision": "aac7a640c059ee7f46c35c2019434117",
+    "url": "/static/media/IRANSansWeb_Light.aac7a640.woff"
+  },
+  {
+    "revision": "e5b2acaef933a97fa3fb026d8ee73d46",
+    "url": "/static/media/IRANSansWeb_Light.e5b2acae.ttf"
+  },
+  {
+    "revision": "7bfa046de6ec101d790ba8a54886dc9a",
+    "url": "/static/media/IRANSansWeb_Medium.7bfa046d.woff"
+  },
+  {
+    "revision": "7c1c84da23760b0d3f59f8de58c47605",
+    "url": "/static/media/IRANSansWeb_Medium.7c1c84da.woff2"
+  },
+  {
+    "revision": "894895856574b0e65e2739d1a2d1de42",
+    "url": "/static/media/IRANSansWeb_Medium.89489585.ttf"
+  },
+  {
+    "revision": "c18ca268db923d61b1c9f06415f4c143",
+    "url": "/static/media/IRANSansWeb_Medium.c18ca268.eot"
+  },
+  {
+    "revision": "2097362beeb263fdd902ad249202cd79",
+    "url": "/static/media/IRANSansWeb_UltraLight.2097362b.woff"
+  },
+  {
+    "revision": "e0e6c92f90cf30782f81d80a37c22c54",
+    "url": "/static/media/IRANSansWeb_UltraLight.e0e6c92f.woff2"
+  },
+  {
+    "revision": "eb6bb7948783b20ce01dbb2d103db8df",
+    "url": "/static/media/IRANSansWeb_UltraLight.eb6bb794.eot"
+  },
+  {
+    "revision": "f8a4f6910724c0373ffd38add5cea6ac",
+    "url": "/static/media/IRANSansWeb_UltraLight.f8a4f691.ttf"
+  },
+  {
+    "revision": "41e28634ed431c6cb13a8ffc706a58e7",
+    "url": "/static/media/profPic1.41e28634.jpg"
+  },
+  {
+    "revision": "30ba47e8e2c934b9156df2311392143b",
+    "url": "/static/media/sara-fotros-daf.30ba47e8.jpg"
+  },
+  {
+    "revision": "91a5be78c140a037f94af064fe5db2a2",
+    "url": "/static/media/scr2.91a5be78.png"
+  },
+  {
+    "revision": "65c31731672fbd55c24a6ea5132fe2af",
+    "url": "/static/media/sfJob.65c31731.jpg"
+  },
+  {
+    "revision": "62d7f47ffbfa862d30c116ebfce41f6d",
+    "url": "/static/media/tnk.62d7f47f.jpeg"
+  }
+]);
